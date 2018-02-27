90851a3ad4210cff4dca242fb6a63d6fcd4aed65
ReluctusB <endahallahan@comcast.net>
2018-02-27 15:14:08 -0600

    Update DiscordVoicePrompter.user.js

--- DiscordVoicePrompter.user.js
+++ DiscordVoicePrompter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Voice Prompter
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.3
 // @description  Adds a prompt when trying to enter a voice channel
 // @author       RB
 // @match        https://discordapp.com/*
@@ -11,14 +11,14 @@
 // @downloadURL  https://raw.githubusercontent.com/ReluctusB/Discord-Voice-Prompter/master/DiscordVoicePrompter.user.js
 // ==/UserScript==
 
-//initiates prompt, simulates a click event on chat is prompt is accepted.
+//Initiates prompt, simulates a click event on chat is prompt is accepted.
 function askNicely(chat) {
     if(window.confirm("Join voice chat?")) {
         chat.click();
     }
 }
 
-//covers voice chat channels with 'obscurers' that call askNicely when clicked.
+//Covers voice chat channels with 'obscurers' that call askNicely when clicked.
 function addObscurity() {
     var chatlist = document.getElementsByClassName("wrapperDefaultVoice-2ud9mj");
     console.log(chatlist);
@@ -26,7 +26,7 @@ function addObscurity() {
         var cover = document.createElement("DIV");
         cover.style.height = "34px";
         cover.style.width = "100%";
-        cover.style.zIndex = 5000;
+        cover.style.zIndex = 100;
         cover.style.position="absolute";
         cover.id = i.toString();
         cover.addEventListener("click",function(){askNicely(this.nextElementSibling);});
@@ -34,15 +34,29 @@ function addObscurity() {
     }
 }
 
-//sets up initial obscurers and adds click events to call addObscurers on guilds.
+//Puts click events to call addObscurity on everything else that needs it within a guild itself
+function setUpGuild() {
+    addObscurity();
+    //Handles category dropdowns
+    if (document.getElementsByClassName("containerDefault-1bbItS")[0]){
+        var categorylist = document.getElementsByClassName("containerDefault-1bbItS");
+        for (let i = 0; i < categorylist.length; i++) {
+            categorylist[i].addEventListener("click", function(){setTimeout(addObscurity,150);}, false);
+        }
+    }
+    //Handles scrolling down a channel list
+    document.getElementsByClassName("scroller-fzNley")[0].addEventListener("scroll", function(){setTimeout(addObscurity,150);}, false);
+}
+
+//Calls initial setUpGuild and adds click events to call setUpGuild on guilds.
 window.addEventListener("load", function(){setTimeout(function() {
     if (document.getElementsByClassName('guild')[0]){
-        addObscurity();
+        setUpGuild();
         var guildlist = document.getElementsByClassName("guild");
         for (let i = 0; i < guildlist.length; i++) {
             guildlist[i].addEventListener("click", function(){setTimeout(function(){
                 if (document.getElementsByClassName("wrapperDefaultVoice-2ud9mj")[0]){
-                addObscurity();}
+                setUpGuild();}
             },150);});
         }
     }
