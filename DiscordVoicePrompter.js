2acb3c5add80ee74c8e69a8c3e54e36aad5318ea
ReluctusB <endahallahan@comcast.net>
2018-02-27 00:55:58 -0600

    Create DiscordVoicePrompter.js

---
+++ DiscordVoicePrompter.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Discord Voice Prompter
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Adds a prompt when trying to enter a voice channel
+// @author       RB
+// @match        https://discordapp.com/*
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/ReluctusB/Discord-Voice-Prompter/master/DiscordVoicePrompter.user.js
+// @downloadURL  https://raw.githubusercontent.com/ReluctusB/Discord-Voice-Prompter/master/DiscordVoicePrompter.user.js
+// ==/UserScript==
+
+function askNicely(chat) {
+    if(window.confirm("Join voice chat?")) {
+        chat.click();
+    }
+}
+
+function addListeners() {
+    if (document.getElementsByTagName('textarea')[0]){
+        var chatlist = document.getElementsByClassName("wrapperDefaultVoice-2ud9mj");
+        for (var i = 0; i < chatlist.length; i++) {
+            chatlist[i].classList.add("voice");
+            var cover = document.createElement("DIV");
+            cover.style.height = "34px";
+            cover.style.width = "100%";
+            cover.style.zIndex = 5000;
+            cover.style.position="absolute";
+            cover.id = i.toString();
+            cover.addEventListener("click",function(){askNicely(this.nextElementSibling);});
+            chatlist[i].parentElement.insertBefore(cover, chatlist[i]);
+        }
+    }else{
+        setTimeout(addListeners,1000);
+    }
+}
+
+window.addEventListener("load", addListeners, false);
+window.addEventListener("click", addListeners, false);
